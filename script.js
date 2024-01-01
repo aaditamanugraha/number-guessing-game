8348a544bcc0545361e26620d0c6b2bd03b59cb4
aaditamanugraha <adhityaaditaman@gmail.com>
2024-01-01 21:51:39 +0800

    Set reset game mechanism

--- script.js
+++ script.js
@@ -51,3 +51,25 @@ function setGameOver() {
   document.body.appendChild(resetButton);
   resetButton.addEventListener("click", resetGame);
 }
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (const resetPara of resetParas) {
+    resetPara.textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+guessSubmit.addEventListener("click", checkGuess);
